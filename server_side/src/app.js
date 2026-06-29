17c4692db78e50fe760554fa74ee87d39a5396f2
Sahil Chaudhary <sahilchaudhary975832@gmail.com>
2026-06-30 02:49:32 +0530

    Add health endpoint

--- server_side/src/app.js
+++ server_side/src/app.js
@@ -15,8 +15,18 @@ app.use(cors({
 
 app.use(express.json())
 
+// adding one health route for uptime bot
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    service: "backend-sss",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+  });
+});
 // This is the place for the routes Sweety, I am adding one route of the auth, rest you may add or let me know
 app.use('/api/auth', require('./modules/auth/auth.routes'))
+
 
 const meetingRoutes = require('./modules/meeting/meeting.routes');
 app.use('/api/meetings', meetingRoutes);
